bcf1b0767aa343658c33a6e233022606dc9dd60c
qiwentao <1547936042@qq.com>
2023-01-18 17:55:33 +0800

    init

---
+++ Step5/cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,27 @@
+# CMake generated Testfile for 
+# Source directory: /home/qiwentao/project/camke_study/cmake-3.25.1-tutorial-source/Step5
+# Build directory: /home/qiwentao/project/camke_study/cmake-3.25.1-tutorial-source/Step5/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(Runs "/home/qiwentao/project/camke_study/cmake-3.25.1-tutorial-source/Step5/cmake-build-debug/Tutorial" "25")
+set_tests_properties(Runs PROPERTIES  _BACKTRACE_TRIPLES "/home/qiwentao/project/camke_study/cmake-3.25.1-tutorial-source/Step5/CMakeLists.txt;56;add_test;/home/qiwentao/project/camke_study/cmake-3.25.1-tutorial-source/Step5/CMakeLists.txt;0;")
+add_test(Usage "/home/qiwentao/project/camke_study/cmake-3.25.1-tutorial-source/Step5/cmake-build-debug/Tutorial")
+set_tests_properties(Usage PROPERTIES  PASS_REGULAR_EXPRESSION "Usage:.*number" _BACKTRACE_TRIPLES "/home/qiwentao/project/camke_study/cmake-3.25.1-tutorial-source/Step5/CMakeLists.txt;62;add_test;/home/qiwentao/project/camke_study/cmake-3.25.1-tutorial-source/Step5/CMakeLists.txt;0;")
+add_test(StandardUse "/home/qiwentao/project/camke_study/cmake-3.25.1-tutorial-source/Step5/cmake-build-debug/Tutorial" "4")
+set_tests_properties(StandardUse PROPERTIES  PASS_REGULAR_EXPRESSION "4 is 2" _BACKTRACE_TRIPLES "/home/qiwentao/project/camke_study/cmake-3.25.1-tutorial-source/Step5/CMakeLists.txt;71;add_test;/home/qiwentao/project/camke_study/cmake-3.25.1-tutorial-source/Step5/CMakeLists.txt;0;")
+add_test(Comp4 "/home/qiwentao/project/camke_study/cmake-3.25.1-tutorial-source/Step5/cmake-build-debug/Tutorial" "4")
+set_tests_properties(Comp4 PROPERTIES  PASS_REGULAR_EXPRESSION "4 is 2" _BACKTRACE_TRIPLES "/home/qiwentao/project/camke_study/cmake-3.25.1-tutorial-source/Step5/CMakeLists.txt;79;add_test;/home/qiwentao/project/camke_study/cmake-3.25.1-tutorial-source/Step5/CMakeLists.txt;83;do_test;/home/qiwentao/project/camke_study/cmake-3.25.1-tutorial-source/Step5/CMakeLists.txt;0;")
+add_test(Comp9 "/home/qiwentao/project/camke_study/cmake-3.25.1-tutorial-source/Step5/cmake-build-debug/Tutorial" "9")
+set_tests_properties(Comp9 PROPERTIES  PASS_REGULAR_EXPRESSION "9 is 3" _BACKTRACE_TRIPLES "/home/qiwentao/project/camke_study/cmake-3.25.1-tutorial-source/Step5/CMakeLists.txt;79;add_test;/home/qiwentao/project/camke_study/cmake-3.25.1-tutorial-source/Step5/CMakeLists.txt;84;do_test;/home/qiwentao/project/camke_study/cmake-3.25.1-tutorial-source/Step5/CMakeLists.txt;0;")
+add_test(Comp5 "/home/qiwentao/project/camke_study/cmake-3.25.1-tutorial-source/Step5/cmake-build-debug/Tutorial" "5")
+set_tests_properties(Comp5 PROPERTIES  PASS_REGULAR_EXPRESSION "5 is 2.236" _BACKTRACE_TRIPLES "/home/qiwentao/project/camke_study/cmake-3.25.1-tutorial-source/Step5/CMakeLists.txt;79;add_test;/home/qiwentao/project/camke_study/cmake-3.25.1-tutorial-source/Step5/CMakeLists.txt;85;do_test;/home/qiwentao/project/camke_study/cmake-3.25.1-tutorial-source/Step5/CMakeLists.txt;0;")
+add_test(Comp7 "/home/qiwentao/project/camke_study/cmake-3.25.1-tutorial-source/Step5/cmake-build-debug/Tutorial" "7")
+set_tests_properties(Comp7 PROPERTIES  PASS_REGULAR_EXPRESSION "7 is 2.645" _BACKTRACE_TRIPLES "/home/qiwentao/project/camke_study/cmake-3.25.1-tutorial-source/Step5/CMakeLists.txt;79;add_test;/home/qiwentao/project/camke_study/cmake-3.25.1-tutorial-source/Step5/CMakeLists.txt;86;do_test;/home/qiwentao/project/camke_study/cmake-3.25.1-tutorial-source/Step5/CMakeLists.txt;0;")
+add_test(Comp25 "/home/qiwentao/project/camke_study/cmake-3.25.1-tutorial-source/Step5/cmake-build-debug/Tutorial" "25")
+set_tests_properties(Comp25 PROPERTIES  PASS_REGULAR_EXPRESSION "25 is 5" _BACKTRACE_TRIPLES "/home/qiwentao/project/camke_study/cmake-3.25.1-tutorial-source/Step5/CMakeLists.txt;79;add_test;/home/qiwentao/project/camke_study/cmake-3.25.1-tutorial-source/Step5/CMakeLists.txt;87;do_test;/home/qiwentao/project/camke_study/cmake-3.25.1-tutorial-source/Step5/CMakeLists.txt;0;")
+add_test(Comp-25 "/home/qiwentao/project/camke_study/cmake-3.25.1-tutorial-source/Step5/cmake-build-debug/Tutorial" "-25")
+set_tests_properties(Comp-25 PROPERTIES  PASS_REGULAR_EXPRESSION "-25 is (-nan|nan|0)" _BACKTRACE_TRIPLES "/home/qiwentao/project/camke_study/cmake-3.25.1-tutorial-source/Step5/CMakeLists.txt;79;add_test;/home/qiwentao/project/camke_study/cmake-3.25.1-tutorial-source/Step5/CMakeLists.txt;88;do_test;/home/qiwentao/project/camke_study/cmake-3.25.1-tutorial-source/Step5/CMakeLists.txt;0;")
+add_test(Comp0.0001 "/home/qiwentao/project/camke_study/cmake-3.25.1-tutorial-source/Step5/cmake-build-debug/Tutorial" "0.0001")
+set_tests_properties(Comp0.0001 PROPERTIES  PASS_REGULAR_EXPRESSION "0.0001 is 0.01" _BACKTRACE_TRIPLES "/home/qiwentao/project/camke_study/cmake-3.25.1-tutorial-source/Step5/CMakeLists.txt;79;add_test;/home/qiwentao/project/camke_study/cmake-3.25.1-tutorial-source/Step5/CMakeLists.txt;89;do_test;/home/qiwentao/project/camke_study/cmake-3.25.1-tutorial-source/Step5/CMakeLists.txt;0;")
+subdirs("MathFunctions")
---
+++ Step8/MathFunctions/mysqrt.cxx
@@ -0,0 +1,28 @@
+#include <iostream>
+
+#include "MathFunctions.h"
+#include "Table.h"
+// a hack square root calculation using simple operations
+double mysqrt(double x) {
+    if (x <= 0) {
+        return 0;
+    }
+
+    // if we have both log and exp then use them
+    double result = x;
+
+    if (x>=1&&x<=10) {
+        std::cout<<"Use table"<<std::endl;
+        result=sqrtTable[static_cast<int>(x)];
+    }
+    // do ten iterations
+    for (int i = 0; i < 10; ++i) {
+        if (result <= 0) {
+            result = 0.1;
+        }
+        double delta = x - (result * result);
+        result = result + 0.5 * delta / result;
+        std::cout << "Computing sqrt of " << x << " to be " << result << std::endl;
+    }
+    return result;
+}
